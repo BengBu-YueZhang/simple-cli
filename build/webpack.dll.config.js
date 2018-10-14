6b776ca9574d5d5f1ab22daa68f4d58b05f9cfbb
BengBu-YueZhang <zhangyue_coder@163.com>
2018-10-14 12:32:21 +0800

    add dll

---
+++ build/webpack.dll.config.js
@@ -0,0 +1,26 @@
+var path = require('path')
+var DllPlugin = require('webpack/lib/DllPlugin')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    // vue全家桶生成动态链接库
+    "vue": ['vue', 'vuex', 'vue-router']
+  },
+  output: {
+    // 动态链接库的名称
+    filename: '[name].dll.js',
+    path: path.resolve(__dirname, 'dist'),
+    // 动态链接库全局变量的名称
+    library: '_dll_[name]'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new DllPlugin({
+      // 全局变量的名称
+      name: '_dll_[name]',
+      // 描述动态链接库的json文件的名称
+      path: path.join(__dirname, 'dist', '[name].manifest.json'),
+    })
+  ]
+}
---
+++ build/webpack.test.config.js
@@ -0,0 +1,86 @@
+var merge = require('webpack-merge')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+var BaseConfig = require('./webpack.base.config')
+var DllReferencePlugin = require('webpack/lib/DllReferencePlugin')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = merge(BaseConfig, {
+  mode: 'production',
+
+  devtool: '#eval-source-map',
+
+  optimization: {
+    minimizer: [
+      // js压缩
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true
+      }),
+      // css压缩
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessorOptions: {
+          safe: true,
+          autoprefixer: { disable: true },
+          mergeLonghand: false,
+          discardComments: {
+            removeAll: true
+          }
+        },
+        canPrint: true
+      })
+    ]
+  },
+
+  splitChunks: {
+    cacheGroups: {
+      default: false,
+      vendors: false,
+      // 第三方模块
+      vendor: {
+        name: 'vendors-chunk',
+        chunks: 'all',
+        test: /[\\/]node_modules[\\/]/,
+        priority: 20  
+      },
+      // 公共的模块
+      common: {
+        name: 'common-chunk',
+        chunks: 'all',
+        minChunks: 2,
+        test: path.resolve(__dirname, './src/components'),
+        priority: 10,
+        enforce: true,
+        reuseExistingChunk: true
+      }
+    }
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      exclude: ['dll']
+    }),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, './dist/index.html'),
+      template: path.resolve(__dirname, './public/index.html')
+    }),
+    new DllReferencePlugin({
+      manifest: require('./dist/dll/vue.manifest.json'),
+    }),
+    // 添加dll文件, 提升打包速度
+    new AddAssetHtmlPlugin([
+      {
+        filepath: path.resolve(__dirname, './dist/dll/*.dll.js'),
+        includeSourcemap: false,
+        publicPath: './dll',
+        outputPath: '/dll'
+      }
+    ]),
+    new MiniCssExtractPlugin(),
+    new BundleAnalyzerPlugin()
+  ]
+})
